5a96c0930d1d9eef4dcc119acb8b3705c5351a51
dyikwong <56622549+dyikwong@users.noreply.github.com>
2022-01-28 22:43:53 -0800

    c

--- csvreader.js
+++ csvreader.js
@@ -3,7 +3,7 @@ function csvToArray(str, delimiter = ",") {
     // slice from start of text to the first \n index
     // use split to create an array from string by delimiter
     const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-    if (headers[headers.length-1] == "url\r") {
+    if (headers[headers.length-1] == "url") {
         headers[headers.length-1] = "url";
     }
 
